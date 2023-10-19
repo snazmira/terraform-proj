b0aa033f25158baf4fa1a56bfca9f148f5b513d6
azmira <your_email_address@example.com>
2023-10-19 12:55:06 +0800

    added random_id

--- main.tf
+++ main.tf
@@ -77,6 +77,15 @@ resource "azurerm_network_interface_security_group_association" "example" {
   network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
 
+# Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
 
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
